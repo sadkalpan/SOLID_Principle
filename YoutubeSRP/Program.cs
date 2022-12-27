b6a41376e184e3e699275030289d3248326f017f
sadkalpan <sadkalpan@gmail.com>
2022-12-27 21:11:15 +0300

    Add project files.

---
+++ YoutubeSRP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YoutubeSRP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

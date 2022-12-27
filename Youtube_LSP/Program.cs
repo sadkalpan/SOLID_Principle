3f429558143c6bbca8587e3674347f2ef8bfd1e8
sadkalpan <sadkalpan@gmail.com>
2022-12-28 00:04:26 +0300

    construct first liskov prinnciple project

--- Youtube_LSP/Program.cs
+++ Youtube_LSP/Program.cs
@@ -6,7 +6,47 @@ namespace Youtube_LSP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Customer engin = new Customer
+            //{
+            //    Id = 1,
+            //    FirstName = "Engin",
+            //    LastName = "Demirog",
+            //    TcNo = "12345"
+            //};
+
+            //Customer company = new Customer
+            //{
+            //    Id = 2,
+            //    FirstName = "Abc Cosmetics",
+            //    LastName = null,
+            //    TcNo= "12345678"//vergi no
+            //};
         }
     }
+    
+    class Customer
+    {
+        public int Id { get; set; }
+    }
+
+    class RealCustomer : Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TcNo { get; set; }
+
+    }
+    class CorporateCustomer : Customer
+    {
+        public string TaxNo { get; set; }
+        public string Title { get; set; }
+    }
+
+    //class Customer
+    //{
+    //    public int Id { get; set; }
+    //    public string FirstName { get; set; }
+    //    public string LastName { get; set; }
+    //    public string TcNo { get; set; }
+    //}
 }

373c4bdc46b50a4e8cba1b22eda316f723c36dd0
sadkalpan <sadkalpan@gmail.com>
2022-12-27 21:56:07 +0300

    Commit first Single Responsibility Principle project

--- YoutubeSRP/Program.cs
+++ YoutubeSRP/Program.cs
@@ -9,4 +9,49 @@ namespace YoutubeSRP
            
         }
     }
+
+    class CustomerManager
+    {
+        //ASPECTED ORIENTED PROGRAMİNG
+        /*
+        public void TransactionOperation()
+        {
+            //LOG
+            //VALIDATION
+            //CACHİNG
+            //AUTH
+            //PERFORMANCE
+            MyContext context = new MyContext();
+            context.Update();
+            context.Insert();
+        }*/
+        private void Insert()
+        { 
+        }
+        private void Update()
+        {
+
+        }
+    }
+
+    class PersonManager
+    {
+        public void Add()
+        {
+            MyContext context = new MyContext();
+            context.Insert();
+        }
+    }
+    internal class MyContext
+    {
+        internal void Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
